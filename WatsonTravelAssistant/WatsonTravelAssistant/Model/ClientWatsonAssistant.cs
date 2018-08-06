5389b9116f699b5e7f0c7d55a96d31e6e01b58e4
Giuseppe Pennisi <giuseppe.pennisi11@gmail.com>
2018-08-06 15:12:04 +0200

    created Model-View-ViewModel Schema. Addedd classess.

--- WatsonTravelAssistant/WatsonTravelAssistant/Model/ClientWatsonAssistant.cs
+++ WatsonTravelAssistant/WatsonTravelAssistant/Model/ClientWatsonAssistant.cs
@@ -4,6 +4,7 @@ using System.Text;
 using IBM.WatsonDeveloperCloud.Conversation.v1;
 using IBM.WatsonDeveloperCloud.Conversation.v1.Model;
 using Newtonsoft.Json;
+using WatsonTravelAssistant.Model;
 
 namespace WatsonTravelAssistant.utils
 {
@@ -12,13 +13,16 @@ namespace WatsonTravelAssistant.utils
         private ConversationService _conversation;
         private string _workspaceID;
         private dynamic _questionContext = null;
-        public ClientWatsonAssistant(string url, string username, string password, string workspaceId)
+        public ClientWatsonAssistant()
         {
-            _conversation = new ConversationService(username, password, "2018-02-16");
-            _conversation.SetEndpoint(url);
-            _workspaceID = workspaceId;
+            _conversation = new ConversationService("afcea3f6 - 1a23 - 4f95 - aa62 - 51304447f1a2", "p0nzObAmUj42", "2018-02-16");
+            _conversation.SetEndpoint("https://gateway.watsonplatform.net/assistant/api");
+            _workspaceID = "965c14dd - 27fd - 4455 - a16e - fdd1196de10a";
             
         }
+
+       public Action<Message> MessageAdded { get; set; }
+
         public void CallConversation(string inputText)
         {
             MessageRequest messageRequest = new MessageRequest()
@@ -36,6 +40,7 @@ namespace WatsonTravelAssistant.utils
                 messageRequest.Context.ConversationId = _questionContext.conversation_id;
                 messageRequest.Context.System = _questionContext.system;
             }
+            
             var result = JsonConvert.SerializeObject(_conversation.Message(_workspaceID, messageRequest), Formatting.Indented);
             
             Console.WriteLine(string.Format("result: {0}", result));
---
+++ WatsonTravelAssistant/WatsonTravelAssistant/View/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WatsonTravelAssistant.ModelView;
+using WatsonTravelAssistant.utils;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WatsonTravelAssistant
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+	{
+        MainChatViewModel vm;
+        public MainPage()
+        {
+            InitializeComponent();
+            Title = "#general";
+            BindingContext = Vm = new MainChatViewModel();
+
+
+            Vm.Messages.CollectionChanged += (sender, e) =>
+            {
+                var target = Vm.Messages[Vm.Messages.Count - 1];
+                MessagesListView.ScrollTo(target, ScrollToPosition.End, true);
+            };
+
+        }
+
+        internal MainChatViewModel Vm { get => vm; set => vm = value; }
+
+        void MyListView_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            MessagesListView.SelectedItem = null;
+        }
+
+        void MyListView_OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            MessagesListView.SelectedItem = null;
+
+        }
+    }
+
+
+}
